61dd404f45a9f5ada14a889db414bbcf503f5da4
Evan <evanbinu17@gmail.com>
2023-12-08 17:25:32 +0530

    Tasks

---
+++ TASK2.py
@@ -0,0 +1,18 @@
+import re
+
+def check_ethereum(address):
+    
+    ethereum_address_pattern = re.compile(r'^0x[a-fA-F0-9]{40}$')
+
+    if ethereum_address_pattern.match(address):
+        return True
+    else:
+        return False
+
+
+ethereum_address = input("Enter your Ethereum Address:")
+
+
+result = check_ethereum(ethereum_address)
+print(result)
+
